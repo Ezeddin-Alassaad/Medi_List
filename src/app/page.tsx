719481f2dde5042d00be852f286ee16ef15c88f1
abdulsalam_alabdullah669 <abdulsalamalabdullah05@gmail.com>
2025-01-01 16:27:19 +0300

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to NextJs</p>      
+    </div>
+  )
+
+export default HomePage
+      <h1>Home Page</h1>
+      <p>Welcome to NextJs</p>
