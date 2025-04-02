85440464430682cfc861a4bd1d4a5955e34b15ba
Ezeddin-Alassaad <ezeddin.alassaad@gmail.com>
2025-04-02 13:08:56 +0300

    new updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-[rgb(227,225,225)] text-black" dir="rtl">
+    <footer className="bg-[rgb(227,225,225)] text-black" dir='ltr' >
       <div className="container mx-auto px-4 py-8 flex flex-col md:flex-row justify-between items-center">
         {/* Logo and Description */}
         <div className="text-center md:text-right mb-4 md:mb-0">
@@ -16,33 +16,21 @@ const Footer = () => {
            <span className="text-green-600">List </span> <LuPill className="mx-1" /> <span className="text-green-600">Medi</span>
           </Link>
           <p className="mt-2 font-extrabold text-sm tracking-wide">
-            مصدرك الموثوق للمعلومات الطبية.
+            مصدرك الموثوق للمعلومات الطبية
           </p>
         </div>
         {/* Social Media Icons */}
-        <div className="flex space-x-reverse space-x-8">
-          <a 
-            href="#"
-            className="hover:text-green-600 transition-colors duration-200"
-            aria-label="Facebook"
-          >
-            <FaFacebookF size={24} className="text-green-600" />
-          </a>
-          <a 
-            href="#"
-            className="hover:text-green-600 transition-colors duration-200"
-            aria-label="Twitter"
-          >
-            <FaTwitter size={24} className="text-green-600" />
-          </a>
-          <a 
-            href="#"
-            className="hover:text-green-600 transition-colors duration-200"
-            aria-label="Instagram"
-          >
-            <FaInstagram size={24} className="text-green-600" />
-          </a>
-        </div>
+      <div className="flex space-x-8">
+        <a href="#" aria-label="Facebook" className="hover:text-green-600 transition-colors duration-200">
+          <FaFacebookF size={24} className="text-green-600" />
+        </a>
+        <a href="#" aria-label="Twitter" className="hover:text-green-600 transition-colors duration-200">
+          <FaTwitter size={24} className="text-green-600" />
+        </a>
+        <a href="#" aria-label="Instagram" className="hover:text-green-600 transition-colors duration-200">
+          <FaInstagram size={24} className="text-green-600" />
+        </a>
+      </div>
       </div>
       <div className="bg-green-700 text-center py-4">
         <p className="text-xs">
